4caae1ad8118cbba59b1b310f33b4c8880ca286a
Parsley3000 <peterkeane2@gmail.com>
2021-11-21 22:05:51 +0000

    Update HelloWorldController.java

--- src/main/java/com/hello/HelloWorldController.java
+++ src/main/java/com/hello/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class HelloWorldController {
 
     @RequestMapping("/")
